b6637e66a4a43951cae6759bc673cad7106be3a6
DavidMariano285868 <paul.mariano@obf.ateneo.edu>
2025-03-01 22:36:17 +0800

    changes to bat file

--- PathfindingAI.bat
+++ PathfindingAI.bat
@@ -1,2 +1,2 @@
-E:\Projects\Pathfinding_AI_project\Path\Scripts\python.exe E:\Projects\Pathfinding_AI_project\TheEmeraldHunterAI\TheEmeraldHunter(Heu).py
+Path\Scripts\python.exe TheEmeraldHunterAI\TheEmeraldHunter(Heu).py
 
